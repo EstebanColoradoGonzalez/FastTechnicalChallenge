987d35457d9aa40cd52033fac7ce7fe5e9c6d2ec
CEIBA\esteban.colorado <estecogn@suramericana.com.co>
2025-07-06 00:25:08 -0500

    feat: create the entity-relationship model objects

--- src/main/java/co/com/ceiba/reto/common/domain/constant/ConstantNumber.java
+++ src/main/java/co/com/ceiba/reto/common/domain/constant/ConstantNumber.java
@@ -2,6 +2,10 @@ package co.com.ceiba.reto.common.domain.constant;
 
 public class ConstantNumber {
     public static final int ZERO = 0;
+    public static final int TEN = 10;
+
+    public static final int DECIMAL_PRECISION = 10;
+    public static final int DECIMAL_SCALE = 2;
 
     public static final Long ONE_LONG = 1L;
 
--- src/main/java/co/com/ceiba/reto/common/domain/constant/ConstantText.java
+++ src/main/java/co/com/ceiba/reto/common/domain/constant/ConstantText.java
@@ -4,5 +4,25 @@ public class ConstantText {
     public static final String EMPTY = "";
     public static final String UNCHECKED = "unchecked";
 
+    public static final String INVENTORY_TABLE = "inventory";
+    public static final String ORDERS_TABLE = "orders";
+    public static final String ORDER_ITEMS_TABLE = "order_items";
+
+    public static final String ORDER_ID = "order_id";
+    public static final String CUSTOMER_ID = "customer_id";
+    public static final String TOTAL_AMOUNT = "total_amount";
+    public static final String STATUS = "status";
+    public static final String PAYMENT_METHOD = "payment_method";
+
+    public static final String PRODUCT_ID = "product_id";
+    public static final String STOCK = "stock";
+    public static final String UNIT_PRICE = "unit_price";
+    public static final String QUANTITY = "quantity";
+
+    public static final String ID = "id";
+
+    public static final String ORDERS_SEQ = "orders_order_id_seq";
+    public static final String ORDER_ITEMS_SEQ = "order_items_id_seq";
+
     private ConstantText() { }
 }
